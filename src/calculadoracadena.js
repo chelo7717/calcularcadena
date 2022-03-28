1094ab0c925d7d47bc5650db68a0b124578fc4eb
Marcelo Salinas <chelo336@gmail.com>
2022-03-28 00:12:34 -0400

    FuncionMejorada Para sumarCadena

--- src/calculadoracadena.js
+++ src/calculadoracadena.js
@@ -1,15 +1,46 @@
-function generarCadena(cadena){
-    let total=0;
-    if(cadena==""){
-        return total;
+function calculadoraCadenas(cadena){
+    let num =0;
+    let separadores =[","]
+    var cadenasSeparadas=cadena;
+    var cadenaDeNumeros=cadenasSeparadas;
+    let sumar=true;
+    let caracter;
+    let elemento;
+    let numero;
+    let secuencia="";
+    let posicionActual=0;
+    while(posicionActual<cadenaDeNumeros.length){
+        numero="";
+        secuencia="";
+        caracter = cadenaDeNumeros[posicionActual];
+        elemento = parseInt(caracter,10);
+        if(isNaN(elemento)){
+            while(isNaN(elemento)){
+                secuencia = secuencia+caracter;
+                posicionActual++;
+                caracter = cadenaDeNumeros[posicionActual];
+                elemento = parseInt(caracter,10);
+            }
+            if(separadores.includes(secuencia) == false){
+                sumar=false;
+            }
+            else{
+                sumar=true;
+            }
+        }
+        else{
+            while(isNaN(elemento)==false){
+                numero = numero+caracter;
+                posicionActual++;
+                caracter = cadenaDeNumeros[posicionActual];
+                elemento = parseInt(caracter,10);
+            }
+            elemento = parseInt(numero,10);
+            if(elemento<1000 && sumar){
+                num = num+elemento;
+            }   
+        }   
     }
-    else
-    {
-        return parseInt(cadena)
-
-    }
-    return total;
-    
-
+    return num;
 }
-export default generarCadena;
+export default calculadoraCadenas;
--- src/calculadorcadena.test.js
+++ src/calculadorcadena.test.js
@@ -1,10 +1,13 @@
-import generarCadena from "./calculadoracadena.js"
+import calculadoraCadenas from "./calculadoracadena.js"
 describe("generar cadena", () => {
   it("deberia generar 0 si la cadena es vacia", () => {
-    expect(generarCadena("")).toEqual(0);
+    expect(calculadoraCadenas("")).toEqual(0);
   });
   it("deberia generar 2 si la cadena es 2", () => {
-    expect(generarCadena("2")).toEqual(2);
+    expect(calculadoraCadenas("2")).toEqual(2);
+  });
+  it("deberia devolver la suma ", () => {
+    expect(calculadoraCadenas("2,3")).toEqual(5);
   });
   
 });
